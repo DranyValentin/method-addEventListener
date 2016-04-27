60ecc820ae89f3d7d3481a9f5be0780bf792ec5b
Drany Valentin <navshop@ukr.net>
2016-04-27 14:28:52 +0300

    Add Method addEventListener(type, callback)

---
+++ addEventListener.js
@@ -0,0 +1,15 @@
+function EventEmitter() {
+	this._events = Object.create(null)
+}
+
+
+
+// С проверкой на уникальность значений
+EventEmitter.prototype.addEventListener = function(type, cb) {
+		if ( type in this._events )
+			this._events[type].add(cb)
+		else {
+			this._events[type] = [cb]
+			this._events[type] = new Set(this._events[type])
+		}
+}
